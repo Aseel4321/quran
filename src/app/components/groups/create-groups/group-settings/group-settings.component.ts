90ca7866e189724e415645f05c69346753624464
Aseel4321 <aseelghaleb2088@gmail.com>
2026-01-12 03:52:27 +0300

    ث

--- src/app/components/groups/create-groups/group-settings/group-settings.component.ts
+++ src/app/components/groups/create-groups/group-settings/group-settings.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupSettingsComponent implements OnInit {
 
+
   constructor() { }
+groupType: string | null = null; // لا يوجد اختيار مبدئي
 
   ngOnInit() {}
 
